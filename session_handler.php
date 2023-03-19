139f43d834a3a30e6bd60ae33839f5fa486360e7
admiral-vulcan <felix.rau@outlook.com>
2023-03-19 14:43:27 +0100

    fixed bug that logged user out quickly. user is now logged in for a week

--- session_handler.php
+++ session_handler.php
@@ -2,18 +2,20 @@
 // My session start function support timestamp management
 function my_session_start($cid = ""): void {
     //ini_set('session.use_strict_mode', 0);
+    session_set_cookie_params(60 * 60 * 24 * 7,"/");  //User stays logged in for a week
     session_start();
     if ($cid !== "") $_SESSION['cid'] = $cid;
-    if (!empty($_SESSION['set_timer']) && $cid == "") $_SESSION['set_timer'] = time();
+    //if (!empty($_SESSION['set_timer']) && $cid == "") $_SESSION['set_timer'] = time();
 
     // Do not allow to use too old session ID
+    /*
     if (!empty($_SESSION['set_timer']) && $_SESSION['set_timer'] < time() - 60 * 60 * 24 * 7) {  //User stays logged in for a week
         session_destroy();
         session_start();
     }
     $_SESSION['set_timer'] = time(); //set and reset time
+*/
 }
-
 function logout(): void {
     $_SESSION['cid'] = null;
     $_SESSION['set_timer'] = null;
