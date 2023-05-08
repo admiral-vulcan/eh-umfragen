b8a04afc857cbc2380cf8177736384bc5baf82a7
admiral-vulcan <felix.rau@outlook.com>
2023-05-08 22:33:32 +0200

    bugfix: quotes' format was wrong in translations

--- zitate.php
+++ zitate.php
@@ -2,12 +2,21 @@
 function zitat($string) {
     $string = strtolower($string);
 
-    $zitat =  "<div class='quote'>";
-    if ($string == "lebenswirklichkeit") $zitat .= "<p><i>Die Wissenschaft ist Teil der Lebenswirklichkeit; es ist das Was, das Wie und Warum von allem in unserer Erfahrung.</i><br>Rachel Carson</p><br>";
-    elseif ($string == "antworten") $zitat .= "<p><i>Wer neue Antworten will, muss neue Fragen stellen.</i><br>Johann Wolfgang von Goethe</p><br>";
-    elseif ($string == "neugierde") $zitat .= "<p><i>Die Neugierde der Kinder ist der Wissensdurst nach Erkenntnis, darum sollte man diese in ihnen fördern und ermutigen.</i><br>John Locke</p><br>";
-    $zitat .=  "</div>";
+    $zitatText = "";
+    $zitatAutorIn ="";
+    if ($string == "lebenswirklichkeit") {
+        $zitatText = "Die Wissenschaft ist Teil der Lebenswirklichkeit; es ist das Was, das Wie und Warum von allem in unserer Erfahrung.";
+        $zitatAutorIn = "Rachel Carson";
+    }
+    elseif ($string == "antworten") {
+        $zitatText = "Wer neue Antworten will, muss neue Fragen stellen.";
+        $zitatAutorIn = "Johann Wolfgang von Goethe";
+    }
+    elseif ($string == "neugierde") {
+        $zitatText = "Die Neugierde der Kinder ist der Wissensdurst nach Erkenntnis, darum sollte man diese in ihnen fördern und ermutigen.";
+        $zitatAutorIn = "John Locke";
+    }
 
-    return translate($zitat, "de", $GLOBALS["lang"]);
+    return "<div class='quote'><p><i>" . translate($zitatText, "de", $GLOBALS["lang"]) . "</i><br>" . $zitatAutorIn . "</p><br></div>";
 }
 ?>
